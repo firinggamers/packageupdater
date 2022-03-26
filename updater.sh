def27658a4f7e7afb8592102063ed0345cd4801c
mohammed farhan <80166282+firinggamers@users.noreply.github.com>
2022-03-26 22:34:11 +0530

    update first commit

---
+++ updater.sh
@@ -0,0 +1,43 @@
+#!/bin/bash 
+
+#script variable defining 
+YUM_CMD=$(which yum)
+APT_GET_CMD=$(which apt-get)
+
+
+if [[ $APT_GET_CMD ]]; then
+     apt-get install -y curl
+     apt-get install -y toilet 
+     apt-get install -y figlet
+     clear 
+
+     echo "THIS SCRIPT WOULD UPDATE YOUR PACKAGES " | toilet -f term -F border --gay
+
+     sleep 1
+fi
+     sleep 1
+
+if [[ $YUM_CMD ]]; then 
+     yum update 
+
+ elif [[ $APT_GET_CMD ]]; then 
+     apt update && apt -y upgrade
+fi
+
+     echo "[ OK ] update done" | toilet -f term -F border --gay
+     
+     sleep 1
+
+     echo "CLEANING AFTER UPDATE [PLEASE DO NOT EXIT]" | toilet -f term -F border --gay
+if [[ $APT_GET_CMD ]]; then
+     apt remove -y toilet 
+     apt remove -y figlet 
+     clear
+     
+     sleep 3
+
+     echo "THANK YOU FOR USING MY SCRIPT" | toilet -f term -F border --gay
+
+     exit 1;
+
+fi
